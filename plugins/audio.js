5058a5046a562abef7c9675e4f0a9d0c9ae659cf
Hammy254 <164391481+Hammy254@users.noreply.github.com>
2024-06-07 14:18:22 +0300

    𝐕𝐄𝐍𝐎𝐌_𝐌𝐃

--- plugins/audio.js
+++ plugins/audio.js
@@ -1,4 +1,4 @@
-const { smd } = require("../lib");h
+const { smd } = require("../lib");
 const { exec } = require("child_process");
 const fs = require("fs");
 async function audioEditor(_0x1ef339, _0x567a0f = "bass", _0x730356 = "") {
